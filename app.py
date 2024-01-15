87d7a139318231b6069ad1daf18dc5891738b58b
Cha0smagick <alekos20@gmail.com>
2024-01-15 12:59:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,108 +1,102 @@
 import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import os
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from PIL import Image
+import textwrap
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
 
-load_dotenv()
-# os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# Function to display formatted Markdown text
+def to_markdown(text):
+    text = text.replace('•', '  *')
+    return textwrap.indent(text, '> ', predicate=lambda _: True)
 
-def get_pdf_text(pdf_docs):
-    text=""
-    for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text+= page.extract_text()
-    return  text
+# Function to generate content using Gemini API
+def generate_gemini_content(prompt, model_name='gemini-pro-vision', image=None):
+    model = genai.GenerativeModel(model_name)
+    if not image:
+        st.warning("Por favor, agrega una imagen para usar el modelo gemini-pro-vision.")
+        return None
 
+    response = model.generate_content([prompt, image])
+    return response
 
-
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    chunks = text_splitter.split_text(text)
-    return chunks
-
-
-def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
-
-
-def get_conversational_chain():
-
-    prompt_template = """
-    Answer the question as detailed as possible from the provided context, everything is spanish, make sure to provide all the details in spanish, if the answer is not in
-    provided context just say, "la respuesta no esta disponible en el contexto", don't provide the wrong answer\n\n
-    Context:\n {context}?\n
-    Question: \n{question}\n
-    Answer:
-    """
-
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
-
-    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
-
-    return chain
-
-
-
-def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    
-    new_db = FAISS.load_local("faiss_index", embeddings)
-    docs = new_db.similarity_search(user_question)
-
-    chain = get_conversational_chain()
-
-    
-    response = chain(
-        {"input_documents":docs, "question": user_question}
-        , return_only_outputs=True)
-
-    print(response)
-    st.write("Reply: ", response["output_text"])
-
-
-
-
+# Streamlit app
 def main():
-    st.set_page_config("Multi PDF Chatbot", page_icon = ":scroll:")
-    st.header("Multi-PDF's 📚 - Chat Agent 🤖 ")
+    st.set_page_config(page_title="Gemini Chatbot", page_icon="🤖")
+    st.title("Gemini Chatbot")
+    st.sidebar.title("Configuración de Gemini")
 
-    user_question = st.text_input("Ask a Question from the PDF Files uploaded .. ✍️📝")
+    # Configurar la API key de Gemini (reemplazar con tu clave de API de Gemini)
+    genai.configure(api_key='TU_CLAVE_API_DE_GEMINI')
 
-    if user_question:
-        user_input(user_question)
+    # Seleccionar el modelo Gemini
+    select_model = st.sidebar.selectbox("Selecciona el modelo", ["gemini-pro", "gemini-pro-vision"])
 
-    with st.sidebar:
-        
-        st.title("📁 PDF File's Section")
-        pdf_docs = st.file_uploader("Upload your PDF Files & \n Click on the Submit & Process Button ", accept_multiple_files=True)
-        if st.button("Submit & Process"):
-            with st.spinner("Processing..."): # user friendly message.
-                raw_text = get_pdf_text(pdf_docs) # get the pdf text
-                text_chunks = get_text_chunks(raw_text) # get the text chunks
-                get_vector_store(text_chunks) # create vector store
-                st.success("Done")
+    # Inicializar la sesión de chat
+    chat = genai.GenerativeModel(select_model).start_chat(history=[])
 
+    # Definir función para obtener respuesta del modelo Gemini
+    def get_response(messages):
+        response = chat.send_message(messages, stream=True)
+        return response
 
-    st.markdown(
-        """
-        <div style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: #0E1117; padding: 15px; text-align: center;">
-            © <a href="https://github.com/gurpreetkaurjethra" target="_blank">Gurpreet Kaur Jethra</a> | Made with ❤️
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+    # Historial del chat
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
+
+    messages = st.session_state["messages"]
+
+    # Mostrar mensajes del historial
+    if messages:
+        for message in messages:
+            role, parts = message.values()
+            if role.lower() == "user":
+                st.markdown(f"Tú: {parts[0]}")
+            elif role.lower() == "model":
+                st.markdown(f"Assistant: {to_markdown(parts[0])}")
+
+    # Entrada del usuario
+    user_input = st.text_area("Tú:")
+
+    # Get optional image input if the model selected is 'gemini-pro-vision'
+    image_file = None
+    if select_model == 'gemini-pro-vision':
+        image_file = st.file_uploader("Sube una imagen (si aplica):", type=["jpg", "jpeg", "png"])
+
+        # Display image if provided
+        if image_file:
+            st.image(image_file, caption="Imagen subida", use_column_width=True)
+
+    # Botón para enviar mensaje o generar contenido según el modelo seleccionado
+    if st.button("Enviar / Generar Contenido"):
+        if user_input:
+            messages.append({"role": "user", "parts": [user_input]})
+            if select_model == 'gemini-pro-vision':
+                # Modelo Gemini Vision Pro seleccionado
+                if not image_file:
+                    st.warning("Por favor, proporciona una imagen para el modelo gemini-pro-vision.")
+                else:
+                    image = Image.open(image_file)
+                    response = generate_gemini_content(user_input, model_name=select_model, image=image)
+                    if response:
+                        if response.candidates:
+                            parts = response.candidates[0].content.parts
+                            generated_text = parts[0].text if parts else "No se generó contenido."
+                            st.markdown(f"Assistant: {to_markdown(generated_text)}")
+                            messages.append({"role": "model", "parts": [generated_text]})
+                        else:
+                            st.warning("No se encontraron candidatos en la respuesta.")
+            else:
+                # Otros modelos Gemini seleccionados
+                response = get_response(user_input)
+
+                # Mostrar respuesta del modelo solo una vez
+                res_text = ""
+                for chunk in response:
+                    res_text += chunk.text
+                st.markdown(f"Assistant: {to_markdown(res_text)}")
+                messages.append({"role": "model", "parts": [res_text]})
+
+    # Actualizar historial de mensajes en la sesión de Streamlit
+    st.session_state["messages"] = messages
 
 if __name__ == "__main__":
     main()
